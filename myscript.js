d28f280db360b355bc487b760e723590323245c0
unknown <vietnh1295@gmail.com>
2019-05-29 13:29:48 +0700

    add myscript.js

---
+++ myscript.js
@@ -0,0 +1,9 @@
+
+    let inputWidth;
+    let inputHeight;
+     inputWidth=prompt("Nhập Width");
+     inputHeight=prompt("Nhâp Height");
+    let Width=parseInt(inputWidth);
+    let Height=parseInt(inputHeight);
+let area=Width*Height;
+document.write("Area="+area)
